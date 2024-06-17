b3c7f26bb082edc9661566ca6201e14f1ced7798
Dhanashree Mhatre <93194086+dhanashreemhatre@users.noreply.github.com>
2024-06-17 12:33:45 +0530

    Update views.py

--- inclusify-backend/store/views.py
+++ inclusify-backend/store/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serilizers import ProductSerializer,CategorySerializer,CartSerializer,CartItemSerializer
-from .models import Product,Category,CartItem,Cart
+from .serilizers import ProductSerializer,CategorySerializer,CartSerializer,MultipleProductSerialzer,CartItemSerializer,ProductReviewSerializer
+from .models import Product,Category,CartItem,Cart,ProductReview
 from rest_framework import status
 from .models import Account
 from rest_framework.permissions import IsAuthenticated
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 class All_products(APIView):
     def get(self,request):
         product=Product.objects.all().order_by('?')
-        serializer=ProductSerializer(product,many=True)
+        serializer=MultipleProductSerialzer(product,many=True)
         return Response(serializer.data)
     
 class All_Categories(APIView):
@@ -26,7 +29,7 @@ class ProductFromThisCategoryApi(APIView):
             category = Category.objects.get(cid=pk)
             products = Product.objects.filter(categories=category)
             category_serializer = CategorySerializer(category)  # Serialize category
-            product_serializer = ProductSerializer(products, many=True)  # Serialize products
+            product_serializer = MultipleProductSerialzer(products, many=True)  # Serialize products
             data = {
                 'category': category_serializer.data,  # Include serialized category data
                 'products': product_serializer.data  # Include serialized products data
@@ -41,15 +44,102 @@ class ProductFromThisCategoryApi(APIView):
         
         
 class ProductDetailAPIView(APIView):
-    def get(self,request,pk):
+    def get(self, request, pk):
         try:
-            print(pk)
-            product_detail=Product.objects.get(pid=pk)
-            serailizer=ProductSerializer(product_detail)
-            return Response(serailizer.data)
-        except:
-            return Response({'error':'Product not found'},status=status.HTTP_404_NOT_FOUND)
+            logger.info(f"Requested PID: {pk}")
+            product_detail = Product.objects.get(pid=pk)
+            product_serializer = ProductSerializer(product_detail)
+            product_review = ProductReview.objects.filter(product=product_detail)
+            product_review_serializer = ProductReviewSerializer(product_review, many=True)
+            data = {
+                'product': product_serializer.data,
+                'review': product_review_serializer.data
+            }
+            logger.info(f"Response Data: {data}")
+            return Response(data)
+        except Product.DoesNotExist:
+            logger.error(f"Product with PID {pk} not found.")
+            return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            logger.error(f"Error occurred: {str(e)}")
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
+class ProductReviewView(APIView):
+    def post(self,request,pk):
+        try:
+            pid=request.data.get('pid')
+            email=request.data.get('email')
+            review=request.data.get('review')
+            rating=request.data.get('rating')
+            subject=request.data.get('subject')
+            user=Account.objects.get(email=email)
+            product=Product.objects.get(pid=pk)
+            
+            #create a review
+            review_obj=ProductReview.objects.create(user=user,product=product,review=review,rating=rating,subject=subject)
+            return Response("Success", status=status.HTTP_201_CREATED)
+        
+        except Product.DoesNotExist:
+            return Response({'error' : 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def put(self, request, pk):
+        try:
+            pid = request.data.get('pid')
+            email = request.data.get('email')
+            review = request.data.get('review')
+            rating = request.data.get('rating')
+            subject = request.data.get('subject')
+
+            user = Account.objects.get(email=email)
+            product = Product.objects.get(pid=pk)
+
+            # Update review
+            try:
+                review_obj = ProductReview.objects.get(user=user, product=product)
+            except ProductReview.DoesNotExist:
+                return Response({'error': 'Review not found'}, status=status.HTTP_404_NOT_FOUND)
+
+            review_data = {
+                'subject': subject,
+                'review': review,
+                'rating': rating
+            }
+            review_serializer = ProductReviewSerializer(review_obj, data=review_data, partial=True)
+
+            if review_serializer.is_valid():
+                review_serializer.save()
+                return Response({'message': 'Success'}, status=status.HTTP_200_OK)
+            return Response(review_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Account.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def delete(self, request, pk):
+        try:
+            email = request.data.get('email')
+            user = Account.objects.get(email=email)
+            product = Product.objects.get(pid=pk)
+
+            # Delete review
+            try:
+                review_obj = ProductReview.objects.get(user=user, product=product)
+                review_obj.delete()
+                return Response({'message': 'Review deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+            except ProductReview.DoesNotExist:
+                return Response({'error': 'Review not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Account.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
 class AddToCartAPIView(APIView):
     def post(self, request, pk):
         try:
